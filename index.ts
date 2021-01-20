24776e99022f4ab35c43045e9f7279da82f210f0
Xss20eee <11365542+o-svetlichnyi@users.noreply.github.com>
2021-01-20 13:39:05 +0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { passContactsToClients, addNewContact, deleteContact } from '.actions';
+
+interface Contact {
+  id: number;
+  name: string;
+  phone: string;
+}
+
+interface ContactsProps {
+  contacts: Contact[];
+}
+
+const initContact = {
+  name: '',
+  phone: ''
+};
+
+const Contacts = ({ contacts = [] }: ContactsProps) => {
+  const [newContact, setNewContact] = useState<{ name: string; phone: string }>(initContact);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(passContactsToClients(contacts));
+  });
+
+  const handleAddContact = () => {
+    dispatch(addNewContact(newContact));
+  };
+
+  const handleDeleteContact = (contactId) => {
+    dispatch(deleteContact(contactId));
+  };
+
+  return (
+    <div className="contact-wrapper">
+      <div className="contacts">
+        {contacts.length &&
+          contacts
+            .sort((a, b) => a.id - b.id)
+            .map(({ name, phone, id }) => {
+              return (
+                <div>
+                  <span>{name}</span>
+                  <span>{phone}</span>
+                  <div onClick={() => handleDeleteContact(id)}>Add contact</div>
+                </div>
+              );
+            })}
+      </div>
+      <div className="new-contact">
+        <input
+          id="name"
+          value={newContact.name}
+          onChange={(e) => setNewContact({ ...newContact, name: e.target.value })}
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          id="phone"
+          value={newContact.phone}
+          onChange={(e) => setNewContact({ ...newContact, phone: e.target.value })}
+        />
+        <label htmlFor="phone">Phone</label>
+        <div onClick={handleAddContact}>Add New Contact</div>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
